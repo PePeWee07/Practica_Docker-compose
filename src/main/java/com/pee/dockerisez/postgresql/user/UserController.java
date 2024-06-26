8f9cf40d0299e3f60cf29971ed058e1e11241d8e
Pepe <pepewee07@gmail.com>
2024-06-26 10:37:14 -0500

    funcional para ambiente de desarrollo docker

--- src/main/java/com/pee/dockerisez/postgresql/user/UserController.java
+++ src/main/java/com/pee/dockerisez/postgresql/user/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
