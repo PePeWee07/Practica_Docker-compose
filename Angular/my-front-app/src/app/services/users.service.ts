9d54100280927b399b6f4f5b181e6860e78cd4f4
Pepe <pepewee07@gmail.com>
2024-06-27 12:04:00 -0500

    fixed

--- Angular/my-front-app/src/app/services/users.service.ts
+++ Angular/my-front-app/src/app/services/users.service.ts
@@ -9,7 +9,7 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
   public getUsers(){
-    return this.http.get('/dockerisez/api/users');
+    return this.http.get('http://javaapp:8080/dockerisez/api/users');
   }
   public getPokemones(id:number){
   return this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
